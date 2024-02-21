690930c3d01e7f1613a96c3c12fb0ac5f82ba814
kurs <kurs@DESKTOP-DOROTA>
2024-02-21 11:59:01 +0100

    Dodaj pliki projektów.

---
+++ P05Shop.API/Program.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P05Shop.API.Models;
+ 
+using P06Shop.Shared.Services.ProductService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddScoped<IProductService,P05Shop.API.Services.ProductService>();
+
+// addScoped - oznacza, że w trakcie jednego requestu będzie istniała tylko jedna instancja klasy ProductService
+// addTransient - oznacza, że obiekt będzie tworzony za każdym razem, gdy odwolujemy się do niego
+// addSingleton - oznacza, że obiekt będzie tworzony tylko raz i będzie istniał tak długo, jak długo istnieje aplikacja
+
+
+// pozwalamy na wykonywanie zapytań z innych domen
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin();
+        builder.AllowAnyMethod();
+        builder.AllowAnyHeader();
+    });
+});
+// gdybysmy chcieli ograniczyc dostep do API tylko do naszej aplikacji, to wtedy:
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("AllowAll", builder =>
+//    {
+//        builder.WithOrigins("http://mySite.pl");
+//        builder.AllowAnyMethod();
+//        builder.AllowAnyHeader();
+//    });
+//});
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ P12MAUI.Client/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace P12MAUI.Client
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
